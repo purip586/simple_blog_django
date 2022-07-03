2bc5397a286bd2683b10ed864cd9fac0c2822011
pradip puri <purip586@gmail.com>
2022-07-04 00:46:53 +0900

    ccc

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,9 +1,9 @@
-from django.views import generic
+from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
 
-class UserRegisterView(generic.CreateView):
+class UserRegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
